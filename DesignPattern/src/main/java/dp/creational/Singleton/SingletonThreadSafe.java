c0c09e3d860f15ffd2ba06d47f8155b0d10e4da9
ASUS <rahul.dhotkar143@gmail.com>
2022-10-10 15:49:47 +0530

    Thread safe singleton

---
+++ DesignPattern/src/main/java/dp/creational/Singleton/SingletonThreadSafe.java
@@ -0,0 +1,34 @@
+package dp.creational.Singleton;
+
+public class SingletonThreadSafe {
+	//Create instance whenever it needed IFF it is null; 
+			/*
+			 * 1 create private constructor 
+			 * 2 create private static variable of the same class 
+			 * 3 create a public static method to access/ get the object of the method
+			 */
+	private static SingletonThreadSafe INSTANCE=null;
+	private SingletonThreadSafe()
+	{
+		
+	}
+	public static synchronized SingletonThreadSafe getInstance()
+	{
+		if(INSTANCE==null)
+		{
+			INSTANCE=new SingletonThreadSafe();
+		}
+		return INSTANCE;
+	}
+
+	public static SingletonThreadSafe getInstanceDoubleChecking() {
+		if (INSTANCE == null) {
+			synchronized (SingletonThreadSafe.class) {
+				if (INSTANCE == null) {
+					INSTANCE = new SingletonThreadSafe();
+				}
+			}
+		}
+		return INSTANCE;
+	}
+}
